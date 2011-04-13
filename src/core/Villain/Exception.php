d2f5960b2595f6521e56eed7d2d5237714bf12c3
Matt Butcher <technosophos@gmail.com>
2011-04-13 17:29:50 -0500

    Added exception classes.

---
+++ src/core/Villain/Exception.php
@@ -0,0 +1,11 @@
+<?php
+/** @file
+ * Contains the villain exception class.
+ */
+
+namespace \Villain;
+
+/**
+ * The VillainException is the base level non-fatal exception for Villain.
+ */
+class Exception extends \FortissimoException {}
---
+++ src/core/Villain/InterruptException.php
@@ -0,0 +1,13 @@
+<?php
+/** @file
+ * This file holds the fatal InterruptException.
+ */
+namespace \Villain;
+
+/**
+ * A fatal exception.
+ *
+ * This is thrown for exception conditions that are grave enough
+ * to stop processing.
+ */
+class InterruptException extends FortissimoInterruptException {}
