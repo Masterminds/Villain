528080221401079da232e857550b78a10c4938f1
Matt Butcher <technosophos@gmail.com>
2011-05-03 14:32:02 -0500

    Added docs.

---
+++ src/core/Villain/Content/__docs.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @file
+ * Documentation for Villain\Content\*
+ */
+
+/**
+ * @defgroup VillainContent Villain Content
+ *
+ * This package defines the generic content system for Villain.
+ */
+
+
+/**
+ * @defgroup VillainContentType Villain Content Types
+ *
+ * This package defines the type system for Villain content.
+ *
+ * The type system is structured as follows:
+ *
+ * A TypeDefinition is composed of zero or more Field objects, where each Field 
+ * describes an individual storage unit (i.e. a Field) on the object. Villain comes with
+ * a variety of predefined fields, including IntegerField for modeling integers, 
+ * StringField to model text data, and GroupField to model a group of other fields.
+ */
+?>
