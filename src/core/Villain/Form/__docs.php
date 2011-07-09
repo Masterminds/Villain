2535fcc2f178bd3e7a6fd3f40d97b18fbce086ad
Matt Farina <matt@mattfarina.com>
2011-07-09 11:29:10 -0400

    Documented a bunch for form todos.

--- src/core/Villain/Form/__docs.php
+++ src/core/Villain/Form/__docs.php
@@ -4,3 +4,27 @@
  * rendered output is passed through the theme system enabling for common
  * markup to be used across all forms.
  */
+
+/**
+ * Form Todos:
+ * @todo make forms and nested containers searchable by name for elements.
+ * @todo Make CSRF not work when we want protection (a real token system)
+ * @todo form validation?
+ *
+ * Elements to create:
+ * @todo Create form element textarea
+ * @todo Create form element password
+ * @todo Create form element select
+ * @todo Create form element checkbox(s)
+ * @todo Create form element date
+ * @todo Create form element radio(s)
+ * @todo Create form element file (to upload a file)
+ * @todo Create form element button
+ * @todo Create form element hidden
+ * @todo Create form element token
+ * @todo Create form element value
+ * @todo Create form element tel (phone)
+ * @todo Create form element tel
+ * @todo Create form element search
+ * @todo Create form element markup (if you want to insert markup in a form someplace)
+ */
