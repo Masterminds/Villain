0996044620aca652d86f5fffee022e4bdcaf8e2c
Matt Butcher <technosophos@gmail.com>
2011-07-18 19:50:31 -0500

    Updated docs for RunExternalPHPUnit.

--- src/core/Villain/Testing/RunExternalPHPUnit.php
+++ src/core/Villain/Testing/RunExternalPHPUnit.php
@@ -18,8 +18,12 @@ class RunExternalPHPUnit extends \BaseFortissimoCommand {
       ->description('Execute PHPUnit as a command on the command line.')
       ->usesParam('tests', 'A path to the test or tests.')
       ->whichHasDefault('./')
+      
       ->usesParam('command', 'The full path to the phpunit command')
       ->whichHasDefault('phpunit')
+      
+      ->usesParam('options', 'A string or an array of commandline options to be passed to phpunit')
+      ->whichHasDefault('--colors')
       
       ->usesParam('basedir', 'The root directory of Villain.')
       ->whichHasDefault('..') // Since this is run in src/, we go up one directory.
@@ -32,13 +36,21 @@ class RunExternalPHPUnit extends \BaseFortissimoCommand {
 
   public function doCommand() {
     
-    $options = '--colors'; //' --include-path ' . $this->param('basedir', '.');
+    $options = $this->param('options', '--colors');
+    if (is_array($options)) {
+      $options = implode(' ', $options);
+    }
     
+    
+    // Hack to run this from within the src/ directory, which is where Fort 
+    // executes
     $command = 'cd ' . $this->param('basedir', '.') . ' && ';
+    
+    // Run phpunit
     $command .= $this->param('command') . ' ' . $options . ' ' . $this->param('tests');
     
     $retval = NULL;
-    print $command . PHP_EOL;
+    //print $command . PHP_EOL;
     system($command, $retval);
     
     return $retval;
