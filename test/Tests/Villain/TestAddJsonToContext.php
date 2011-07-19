dfbeb510c27e731bdeec3bf7e097419e8c95a067
Matt Butcher <technosophos@gmail.com>
2011-07-18 19:43:10 -0500

    Added JSON context unit tests.

---
+++ test/Tests/Villain/TestAddJsonToContext.php
@@ -0,0 +1,28 @@
+<?php
+/** @file
+ *
+ * Defines the class TestAddJsonToContext.
+ *
+ * Created by Matt Butcher on 2011-07-18.
+ */
+require 'VillainTestCase.php';
+/**
+ * Defines TestAddJsonToContext.
+ */
+class TestAddJsonToContext extends VillainTestCase {
+  
+  public function testDoCommand() {
+    $runner = new \Villain\Util\CommandRunner();
+    
+    $klass = '\Villain\Configuration\AddJsonToContext';
+    $params = array('data' => '{"test": "foo", "test2": 1234}');
+    
+    $runner->run($klass, $params);
+    
+    $result = $runner->context();
+    
+    $this->assertEquals('foo', $result->get('test'), 'Check string JSON data.');
+    $this->assertEquals(1234, $result->get('test2'), 'Check integer JSON data.');
+  }
+  
+}
