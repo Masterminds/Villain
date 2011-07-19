1fbd5c2eee037690d6a2f63c5db0759849e86c6c
Matt Butcher <technosophos@gmail.com>
2011-07-19 12:38:18 -0500

    A specific unit test can now be specified.
    The arg:2 value is now used to grab the name of a unit test or
    testing directory.

--- src/config/tools.php
+++ src/config/tools.php
@@ -3,6 +3,6 @@ require_once 'config/commands.php';
 
 Config::request('@test')
   ->doesCommand('phpunit')
-  ->withParam('tests')->whoseValueIs('test/Tests/Villain/')
+  ->withParam('tests')->whoseValueIs('test/Tests/Villain/')->from('arg:2')
   ->whichInvokes('\Villain\Testing\RunExternalPHPUnit')
 ;
