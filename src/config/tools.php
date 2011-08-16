4920b6c3d7869b6f11add5e9512a157b20272aab
Matt Butcher <technosophos@gmail.com>
2011-08-16 16:41:40 -0500

    Added some tricks to unit test before Villain is installed.

--- src/config/tools.php
+++ src/config/tools.php
@@ -1,8 +1,17 @@
 <?php
-require_once 'config/commands.php';
+if (is_readable('config/commands.php')) {
+  require_once 'config/commands.php';
+}
+else {
+  trigger_error("It appears that Villain is not installed.", E_USER_NOTICE);
+}
+
 
 Config::request('@test')
   ->doesCommand('phpunit')
+  // The get:flags is a cheap trick for running inside of IDEs. 
+  // e.g. --get flags=--verbose+--colors
+  ->withParam('options')->whoseValueIs('--colors --verbose')->from('get:flags')
   ->withParam('tests')->whoseValueIs('test/Tests/Villain/')->from('arg:2')
   ->whichInvokes('\Villain\Testing\RunExternalPHPUnit')
 ;
